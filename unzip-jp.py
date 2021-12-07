d56dec19f355ffea071f508153ea15327fc3cd91
kPherox <admin@mail.kr-kp.com>
2021-12-08 03:25:37 +0900

    use getopt

--- unzip-jp.py
+++ unzip-jp.py
@@ -1,38 +1,62 @@
 #!/usr/bin/env python
 
-# Extracts a zip archive while converting file names from Shift-JIS encoding to UTF-8.
+# Extracts a zip archive while converting file names from Shift-JIS to UTF-8.
 #
 # Example:
 #   python unzip-jp.py archive.zip
 #
 #       Creates a directory `archive` and extracts the archive there.
 #
-import zipfile
+from zipfile import ZipFile
 import sys
 import os
+import errno
 import codecs
+import getopt
 
-if len(sys.argv) < 2:
-    print('No archive name.')
-    print('')
-    print('Usage: unzip-jp archive [password]')
-    exit(1)
 
-name = sys.argv[1]
+def print_error(err):
+    print(err)
+    usage()
+    exit(2)
 
-if len(sys.argv) > 2:
-    password = sys.argv[2]
-else:
-    password = None
+
+def usage():
+    print('Usage:\t{0} [-p password] [--] archive'.format(sys.argv[0]))
+
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:],
+                               'hp:',
+                               ['help', 'password='])
+except getopt.GetoptError as err:
+    print_error(err)
+
+password = None
+
+for o, a in opts:
+    if o in ('-p', '--password'):
+        password = a
+    elif o in ('-h', '-?', '--help', '--usage'):
+        usage()
+        print('Help:\t-p, --password: password\n\t-h, --help: print help.')
+        exit()
+    else:
+        assert False, 'unhandled option'
+
+if len(args) < 1:
+    print_error('No archive name.')
+
+name = args[0]
 
 directory = os.path.splitext(os.path.basename(name))[0]
 
 if not os.path.exists(directory):
     os.makedirs(directory)
 
-with zipfile.ZipFile(name, 'r') as z:
+with ZipFile(name, 'r') as z:
     if password:
-        z.setpassword(password.encode('cp850','replace'))
+        z.setpassword(password.encode('cp850', 'replace'))
     for f in z.infolist():
         bad_filename = f.filename
         if bytes != str:
@@ -41,17 +65,17 @@ with zipfile.ZipFile(name, 'r') as z:
             bad_filename = bytes(bad_filename, 'cp437')
         try:
             uf = codecs.decode(bad_filename, 'sjis')
-        except:
+        except ValueError:
             uf = codecs.decode(bad_filename, 'shift_jisx0213')
         # need to print repr in Python 2 as we may encounter UnicodeEncodeError
         # when printing to a Windows console
         print(repr(uf))
-        filename=os.path.join(directory, uf)
+        filename = os.path.join(directory, uf)
         # create directories if necessary
         if not os.path.exists(os.path.dirname(filename)):
             try:
                 os.makedirs(os.path.dirname(filename))
-            except OSError as exc: # Guard against race condition
+            except OSError as exc:  # Guard against race condition
                 if exc.errno != errno.EEXIST:
                     raise
         # don't try to write to directories
